736284e38a3dfe2400a889f4a178989f5295b800
Romoy Headley <romoy@rombot.home>
2016-01-23 09:48:08 -0500

    issue-53 set default value for contributions to prevent bind error

--- client/js/chartSelector.jsx
+++ client/js/chartSelector.jsx
@@ -18,7 +18,7 @@ class ChartSelectorComponent extends React.Component {
         <h4 className="instructions">1. Choose to create a visualization based on a) contributions over time, b) Breakdown of Contributions</h4>
         <select ref="chart" onChange={this._handleChange.bind(this)}>
           <option value="">Choose information set</option>
-          <option value="contributionOverTime">Contributions Over Time</option>
+          <option selected value="contributionOverTime">Contributions Over Time</option>
           <option value="contributorBreakdown">Breakdown of Contributions</option>
           <option value="contributorDendogram">Tree of Contributors</option>
         </select>
