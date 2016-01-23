10d7164bfb3751811be53b896348a29666c87ffa
Romoy Headley <romoy@webs.com>
2016-01-23 10:04:19 -0500

    issue-53 remove duplication of info

--- client/js/chartSelector.jsx
+++ client/js/chartSelector.jsx
@@ -3,28 +3,29 @@ import ReactDOM from 'react-dom';
 import {Input} from 'react-bootstrap';
 
 class ChartSelectorComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  _handleChange() {
-    let chart = ReactDOM.findDOMNode(this.refs.chart).value;
-    this.props.onChartSelected(chart);
-  }
+    _handleChange() {
+        let chart = ReactDOM.findDOMNode(this.refs.chart).value;
+        this.props.onChartSelected(chart);
+    }
 
-  render() {
-    return (
-      <div>
-        <h4 className="instructions">1. Choose to create a visualization based on a) contributions over time, b) Breakdown of Contributions</h4>
-        <select ref="chart" onChange={this._handleChange.bind(this)}>
-          <option value="">Choose information set</option>
-          <option selected value="contributionOverTime">Contributions Over Time</option>
-          <option value="contributorBreakdown">Breakdown of Contributions</option>
-          <option value="contributorDendogram">Tree of Contributors</option>
-        </select>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h4 className="instructions">
+                    <p> 1. Choose to create a visualization based on </p>
+                </h4>
+                <select ref="chart" onChange={this._handleChange.bind(this)}>
+                    <option selected value="contributionOverTime">Contributions Over Time</option>
+                    <option value="contributorBreakdown">Breakdown of Contributions</option>
+                    <option value="contributorDendogram">Tree of Contributors</option>
+                </select>
+            </div>
+        );
+    }
 }
 
 export default ChartSelectorComponent;
